e21ef63ccf1dd7c7c98e10c57e6ff849153251e2
andrewi <andrewi@corrigo.com>
2020-05-13 15:33:50 -0700

    circleci updates

--- build_scripts/deploy_config.sh
+++ build_scripts/deploy_config.sh
@@ -6,9 +6,10 @@
 
 cd functions
 sed -i 's/\(CI_PROJECT_ID\)/'$CI_PROJECT_ID'/' permissions_build.json
-sed -i 's/\(CI_PRIVATE_KEY_ID\)/'$CI_PRIVATE_KEY_ID'/' permissions_build.json
 sed -i 's/\(CI_PRIVATE_KEY_VALUE\)/'$CI_PRIVATE_KEY_VALUE'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_EMAIL\)/'$CI_CLIENT_EMAIL'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_ID\)/'$CI_CLIENT_ID'/' permissions_build.json
+ESCAPED_REPLACE=$(echo $CI_PRIVATE_KEY_ID | sed -e 's/[\/&]/\\&/g')
+sed -i 's/\(CI_PRIVATE_KEY_ID\)/'$ESCAPED_REPLACE'/' permissions_build.json
 
 cat permissions_build.json
