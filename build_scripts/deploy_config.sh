47baf1ca0e5e7b8aac7687a5c72ebf81ce3b8fba
andrewi <andrewi@corrigo.com>
2020-05-13 14:34:31 -0700

    update key

--- build_scripts/deploy_config.sh
+++ build_scripts/deploy_config.sh
@@ -7,8 +7,8 @@
 cd functions
 sed -i 's/\(CI_PROJECT_ID\)/'$CI_PROJECT_ID'/' permissions_build.json
 sed -i 's/\(CI_PRIVATE_KEY_ID\)/'$CI_PRIVATE_KEY_ID'/' permissions_build.json
-sed -i 's/\(CI_PRIVATE_KEY\)/'$CI_PRIVATE_KEY'/' permissions_build.json
+sed -i 's/\(CI_PRIVATE_KEY_VALUE\)/'$CI_PRIVATE_KEY_VALUE'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_EMAIL\)/'$CI_CLIENT_EMAIL'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_ID\)/'$CI_CLIENT_ID'/' permissions_build.json
-sed -i 's/\(CI_CLIENT_X509_CERT_URL\)/'$CI_CLIENT_X509_CERT_URL'/' permissions_build.json
+
 cat permissions_build.json
