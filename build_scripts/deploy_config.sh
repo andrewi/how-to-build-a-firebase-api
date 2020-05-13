2ddcaaf640722fd7849ffabd9c811dbdb781ccf3
andrewi <andrewi@corrigo.com>
2020-05-13 15:43:56 -0700

    circleci updates

--- build_scripts/deploy_config.sh
+++ build_scripts/deploy_config.sh
@@ -6,10 +6,13 @@
 
 cd functions
 sed -i 's/\(CI_PROJECT_ID\)/'$CI_PROJECT_ID'/' permissions_build.json
-sed -i 's/\(CI_PRIVATE_KEY_VALUE\)/'$CI_PRIVATE_KEY_VALUE'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_EMAIL\)/'$CI_CLIENT_EMAIL'/' permissions_build.json
 sed -i 's/\(CI_CLIENT_ID\)/'$CI_CLIENT_ID'/' permissions_build.json
-ESCAPED_REPLACE=$(echo $CI_PRIVATE_KEY_ID | sed -e 's/[\/&]/\\&/g')
-sed -i 's/\(CI_PRIVATE_KEY_ID\)/'$ESCAPED_REPLACE'/' permissions_build.json
+sed -i 's/\(CI_PRIVATE_KEY_ID\)/'$CI_PRIVATE_KEY_ID'/' permissions_build.json
 
+# sed -i 's/\(CI_PRIVATE_KEY_VALUE\)/'$CI_PRIVATE_KEY_VALUE'/' permissions_build.json
+ESCAPED_REPLACE=$(echo $CI_PRIVATE_KEY_VALUE | sed -e 's/[\/&]/\\&/g')
+sed -i 's/\(CI_PRIVATE_KEY_VALUE\)/'$ESCAPED_REPLACE'/' permissions_build.json
+
+echo $ESCAPED_REPLACE
 cat permissions_build.json
